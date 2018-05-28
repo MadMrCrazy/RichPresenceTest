4fc6b8cde3e7cf6e4f20ac363e523f6908759c67
MadMrCrazy <madmrcrazy@gmail.com>
2018-05-28 19:10:05 +0930

    Massive Cleanup

--- RichTest.py
+++ RichTest.py
@@ -6,10 +6,13 @@ from threading import Thread
 client_id = "434356416304906240"
 RPC = Presence(client_id)
 RPC.connect()
-x = input("What game are you playing?: ")
-y = 0
+x = "At the menu"
 username = input("username: ")
 while True:
-    RPC.update(details="Playing " +x, large_image="madmrcrazy", large_text="PSN: " + username")
-    print("Set Presence @ " + str(y))
-    x = input("What game are you playing?: ")
+    RPC.update(details=x, large_image="madmrcrazy", large_text="PSN: "+username)
+    print("Set Presence")
+    x = input("What game are you playing? : ")
+    if x == "":
+            x = "At the menu"
+    else:
+        x = "Playing " + x
